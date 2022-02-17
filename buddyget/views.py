bebf3e1caecbc680f1f9e781f7f5ff39ae4af4c4
Luigi Minardi <lugminsinlag@gmail.com>
2022-02-17 16:02:13 +0100

    feat: Creating the serializer, the views and the routes (urls)

--- buddyget/views.py
+++ buddyget/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from buddyget.models import Income, Expense
+from buddyget.serializer import IncomeSerializer, ExpenseSerializer
 
-# Create your views here.
+class IncomesViewSet(viewsets.ModelViewSet):
+    """Showing all incomes"""
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+
+class ExpensesViewSet(viewsets.ModelViewSet):
+    """Showing all expenses"""
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
